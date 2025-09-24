fc2fc1d29c32ee6c58700d0d3758053a5a73285d
Adrián Manco Sánchez <adrian.manco@aizon.ai>
2025-09-24 15:10:36 +0200

    feat(python): Implement Filepath Parametrization

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ import sys
 
 from stats import get_book_text, count_words, count_characters, sort_character_count, generate_report
 
-FILEPATH = "books/frankenstein.txt"
-
-def main(args=sys.argv) -> None:
+def main() -> None:
     """
     Entry point of the BookBot application.
     
@@ -17,12 +15,18 @@ def main(args=sys.argv) -> None:
         None
     """
 
-    book_text = get_book_text(FILEPATH)
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    filepath = sys.argv[1]
+
+    book_text = get_book_text(filepath)
     book_word_count = count_words(book_text)
     book_character_count = count_characters(book_text)
     sorted_book_character_count = sort_character_count(book_character_count)
 
-    generate_report(FILEPATH, book_word_count, sorted_book_character_count)
+    generate_report(filepath, book_word_count, sorted_book_character_count)
 
 if __name__ == "__main__":
     main()
